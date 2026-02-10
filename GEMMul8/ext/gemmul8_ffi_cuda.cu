1b13da61d1d7059a94cce43c416e9170d06dd2e1
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2026-02-09 22:02:02 -0700

    refactor: update FFI buffer access from method calls and `typed_data()` to direct member access and `data`.

--- GEMMul8/ext/gemmul8_ffi_cuda.cu
+++ GEMMul8/ext/gemmul8_ffi_cuda.cu
@@ -102,20 +102,20 @@ ffi::Error Gemmul8GemmF64Impl(
     // buffers
     ffi::Buffer<ffi::DataType::F64> A, ffi::Buffer<ffi::DataType::F64> B,
     ffi::ResultBuffer<ffi::DataType::F64> C) {
-  if (A.dimensions().size() != 2 || B.dimensions().size() != 2 ||
-      C->dimensions().size() != 2) {
+  if (A.dimensions.size() != 2 || B.dimensions.size() != 2 ||
+      C->dimensions.size() != 2) {
     return InvalidArgumentError("Only rank-2 matrices supported.");
   }
 
   // Output shape
-  const int64_t m = C->dimensions()[0];
-  const int64_t n = C->dimensions()[1];
+  const int64_t m = C->dimensions[0];
+  const int64_t n = C->dimensions[1];
 
   // Infer k
-  const int64_t k = (transa == 0) ? A.dimensions()[1] : A.dimensions()[0];
+  const int64_t k = (transa == 0) ? A.dimensions[1] : A.dimensions[0];
 
   // Basic consistency check (shape only)
-  const int64_t kB = (transb == 0) ? B.dimensions()[0] : B.dimensions()[1];
+  const int64_t kB = (transb == 0) ? B.dimensions[0] : B.dimensions[1];
   if (kB != k) {
     return InvalidArgumentError("Inner dimension mismatch between A and B.");
   }
@@ -169,18 +169,16 @@ ffi::Error Gemmul8GemmF64Impl(
   if (use_extra_workspace) {
     (void)gemmul8::gemm<double, /*UseExtraWorkspace=*/true>(
         handle, opA, opB, (size_t)m, (size_t)n, (size_t)k, &alpha,
-        (const double *)A.typed_data(), (size_t)lda,
-        (const double *)B.typed_data(), (size_t)ldb, &beta,
-        (double *)C->typed_data(), (size_t)ldc,
+        (const double *)A.data, (size_t)lda, (const double *)B.data,
+        (size_t)ldb, &beta, (double *)C->data, (size_t)ldc,
         (unsigned)num_moduli, (bool)(fastmode != 0), work_rem, workA, workB,
         (bool)(enable_skip_scalA != 0), (bool)(enable_skip_scalB != 0),
         (bool)(skip_scalA != 0), (bool)(skip_scalB != 0));
   } else {
     (void)gemmul8::gemm<double, /*UseExtraWorkspace=*/false>(
         handle, opA, opB, (size_t)m, (size_t)n, (size_t)k, &alpha,
-        (const double *)A.typed_data(), (size_t)lda,
-        (const double *)B.typed_data(), (size_t)ldb, &beta,
-        (double *)C->typed_data(), (size_t)ldc,
+        (const double *)A.data, (size_t)lda, (const double *)B.data,
+        (size_t)ldb, &beta, (double *)C->data, (size_t)ldc,
         (unsigned)num_moduli, (bool)(fastmode != 0), work_rem, workA, workB,
         (bool)(enable_skip_scalA != 0), (bool)(enable_skip_scalB != 0),
         (bool)(skip_scalA != 0), (bool)(skip_scalB != 0));
